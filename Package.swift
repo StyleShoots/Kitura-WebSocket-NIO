93f6ec50c7c494722be8886818a82167ff4143ba
lahari <gplahari@gmail.com>
2024-09-17 16:01:39 +0200

    Update packages

--- Package.swift
+++ Package.swift
@@ -30,7 +30,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/Kitura/Kitura-NIO.git", from: "2.4.200"),
-        .package(url: "https://github.com/Kitura/BlueCryptor.git", from: "1.0.200"),
+        .package(url: "https://github.com/Kitura/BlueCryptor.git", from: "2.0.0"),
         .package(url: "https://github.com/Kitura/Kitura-WebSocket-Compression.git", from: "0.1.200")
     ],
     targets: [
